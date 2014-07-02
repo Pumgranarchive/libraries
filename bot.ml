1546bf18fdc480b3e55eb4aa75d726a0a9a5bf92
Alexandre SADDOUR <alex.saddour@gmail.com>
2014-07-02 20:22:43 +0100

    FREEBASE: ADD: get_topics and json accessors to freebase_http.ml

--- bot.ml
+++ bot.ml
@@ -6,18 +6,25 @@ open Yojson.Basic
 ** Main()
 *)
 
-lwt _ =
-    lwt tamer = Youtube_http.search_video "le fossoyeur" 2
-    (* lwt tamer = *)
-    (* let url_list = ["https://www.youtube.com/watch?v=WFDR6OwFcn4"; *)
-    (*    "https://www.youtube.com/watch?v=a-7MKTjzW_I";"https://www.youtube.com/watch?v=j7uiWGEPxNQ"] in *)
-    (* let id_list = List.map Youtube_http.get_id_from_url url_list *)
-    (* in *)
-    (* Youtube_http.get_videos_from_ids id_list *)
-  in
-  Lwt.return (List.map Youtube_http.print_youtube_video tamer)
+(* lwt  _ = *)
+(*   lwt tamer = Youtube_http.search_video "le fossoyeur" 2 *)
+(*   in *)
+(*   Lwt.return (List.map Youtube_http.print_youtube_video tamer) *)
+
+(* lwt  _ = *)
+(*   lwt tamer = *)
+(*     let url_list = ["https://www.youtube.com/watch?v=qW6D8rYppwY"; *)
+(*                     "https://www.youtube.com/watch?v=ICsryK_w9P0"; *)
+(*                     "https://www.youtube.com/watch?v=j7uiWGEPxNQ"] in *)
+(*     let id_list = List.map Youtube_http.get_id_from_url url_list *)
+(*     in *)
+(*     Youtube_http.get_videos_from_ids id_list *)
+(*   in *)
+(*   Lwt.return (List.map Youtube_http.print_youtube_video tamer) *)
 
 lwt _ =
-  lwt freebase_results = Freebase_http.search "bob" in
-  Lwt.return (Freebase_http.print_json freebase_results)
+  lwt freebase_results = Freebase_http.get_topics "/m/0ndwt2w" in
+  Lwt.return (Freebase_http.print_freebase_object freebase_results)
+  (* Lwt.return () *)
 
+(* NineMuses: /m/0g9tjtf *)
--- freebase_http.ml
+++ freebase_http.ml
@@ -9,32 +9,136 @@
 (*** freebase url ***)
 let api_base_url = "https://www.googleapis.com/freebase/v1/"
 let api_search_url = api_base_url ^ "search?query="
-let api_topic_url = api_base_url ^ "topic/"
-
+let api_topic_url = api_base_url ^ "topic"
 
 (*** url creators ***)
-let create_freebase_search_url request = api_search_url ^ request
+let create_search_url request = api_search_url ^ request
+let create_topic_url ids filter lang =
+  api_topic_url ^ ids
+  ^ "?filter=" ^ (Bfy_helpers.strings_of_list filter "&filter=")
+  ^ "&lang=" ^ lang
 
 (*** json accessor ***)
 let get_result_field json =
-  Yojson.Basic.Util.(to_list (member "result" json))
+  Yojson_wrap.(to_list (member "result" json))
 
 let get_mid_field json =
-  Yojson.Basic.Util.(to_string (member "mid" json))
+  Yojson_wrap.(to_string (member "mid" json))
+
+let get_property_field json =
+  Yojson_wrap.member "property" json
+
+let get_ctdescription_field json =
+  Yojson_wrap.member "/common/topic/description" json
+
+let get_values_field json =
+  Yojson_wrap.(to_list (member "values" json))
+
+let get_value_field json =
+  Yojson_wrap.(to_string (member "value" json))
+
+let get_id_field json =
+  Yojson_wrap.(to_string (member "id" json))
+
+let get_text_field json =
+  Yojson_wrap.(to_string (member "text" json))
+
+let get_ctsocial_media_presence_field json =
+  Yojson_wrap.member "/common/topic/social_media_presence" json
+
+let get_toname_field json =
+  Yojson_wrap.member "/type/object/name" json
+
+let get_totype_field json =
+  Yojson_wrap.member "/type/object/type" json
+
+let get_cttopic_equivalent_webpage_field json =
+  Yojson_wrap.member "/common/topic/topic_equivalent_webpage" json
+
+(*** Unclassed ***)
+let freebase_object_of_json json =
+  let id = get_id_field json in
+  let property = get_property_field json in
+  let sliced_description =
+    let ct_descr = List.hd (get_values_field (get_ctdescription_field property)) in
+    get_text_field ct_descr in
+  let social_media_presences =
+    List.map
+      get_value_field
+      (get_values_field (get_ctsocial_media_presence_field property)) in
+  let name =
+    get_value_field (List.hd (get_values_field (get_toname_field property))) in
+  let types =
+    let create_type json = (get_id_field json, get_text_field json) in
+    List.map create_type (get_values_field (get_totype_field property)) in
+  let rec wiki_url =
+    let rec get_wiki_url =
+      let uri_reg =
+        Str.regexp "\\(https?://\\)?\\(www\\.\\)?en\\.wikipedia.org/wiki/.+" in
+      let is_en_wiki_url url = Str.string_match uri_reg url 0 in
+      function
+      | (url::t)  ->
+        if ((is_en_wiki_url url) = true)
+        then [url]
+        else get_wiki_url t
+      | _       -> []
+    in
+    let url_list =
+      List.map
+        get_value_field
+        (get_values_field (get_cttopic_equivalent_webpage_field property))
+    in
+    get_wiki_url url_list
+  in
+  (id, name, sliced_description, social_media_presences, types, wiki_url)
 
 (*
 ** PUBLIC
 *)
 
+(*** printing ***)
+let print_freebase_object
+    (id, name, sliced_description, sm_presences, types, wiki_url) =
+  let rec string_of_sm = function
+    | (h::t)    -> "\n  -" ^ h ^ (string_of_sm t)
+    | _         -> "" in
+  let rec string_of_type = function
+    | (id, text)::t     ->
+      "\n  -id:\"" ^ id ^ "\"\n  -text:\"" ^ text ^ "\"\n" ^ (string_of_type t)
+    | _                 -> ""
+  in
+  print_endline ("\n<== Freebase object ==>\n"
+    ^"->id:\"" ^ id ^ "\"\n"
+    ^ "->name:\"" ^ name ^ "\"\n"
+    ^ "->sliced_description:\"" ^ sliced_description ^ "\"\n"
+    ^ "->sm_presences:" ^ (string_of_sm sm_presences) ^ "\n"
+    ^ "->types:" ^ (string_of_type types) ^ "\n"
+    ^ "->wiki_url:\"" ^ (List.hd wiki_url) ^ "\"\n")
+
+
+(*** requests ***)
 let search query =
-  let url = create_freebase_search_url query in
+  let url = create_search_url query in
   lwt freebase_json = Http_request_manager.request url
   in
   Lwt.return (freebase_json)
 
-let print_json json =
-  let rec print_mid = function
-    | (h::t)      -> (print_endline (get_mid_field h)); print_mid t
-    | _           -> ()
+(* TODO: ids must become a list *)
+let get_topics ids =
+  let url =
+    create_topic_url
+      ids
+      [
+        "/common/topic/description";
+        "/common/topic/topic_equivalent_webpage";
+        "/type/object/name";
+        "/type/object/type";
+        "/common/topic/description";
+        "/common/topic/social_media_presence"
+      ]
+      "en"
   in
-  print_mid (get_result_field json)
+  lwt freebase_json = Http_request_manager.request ~display_body:false url
+  in
+  Lwt.return (freebase_object_of_json freebase_json)
+
