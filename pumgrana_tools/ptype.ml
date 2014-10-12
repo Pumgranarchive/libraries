c58c7a8cffed86d0c5dc94ca9725d627c0f33a68
ArnaudParant <ard.parant@gmail.com>
2014-10-12 16:28:55 -0500

    [Ptools] is now encoded > and <

--- pumgrana_tools/ptype.ml
+++ pumgrana_tools/ptype.ml
@@ -42,8 +42,8 @@ let replace remove_list replace_list str =
   in
   List.fold_left2 aux str remove_list replace_list
 
-let char_list =    ["/";  ":";  "?";  "=";  "&";  "#";  ";";  " "]
-let encoded_char = ["%2F";"%3A";"%3F";"%3D";"%26";"%23";"%3B";"%20"]
+let char_list =    ["/";  ":";  "?";  "=";  "&";  "#";  ";";  " ";  "<";  ">"]
+let encoded_char = ["%2F";"%3A";"%3F";"%3D";"%26";"%23";"%3B";"%20";"%3C";"%3E"]
 
 let uri_encode url =
   replace char_list encoded_char url
