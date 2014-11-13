cc0937d80b602f06b619f709dda7500cbb6d712e
Alexandre SADDOUR <alex.saddour@gmail.com>
2014-11-13 21:16:31 +0000

    DBPedia: CLEAN + WORDING

--- dbpedia/dbpedia_http.ml
+++ dbpedia/dbpedia_http.ml
@@ -59,14 +59,14 @@ let print_lightweight record =
   )
 
 let print_basic record =
-  let open Dbpedia_record.LightWeight in
+  let open Dbpedia_record.Basic in
   print_endline "--------";
-  (Printf.printf "title: %s\nabstract: %s\nwiki_page: %s\nis_primary_topic_of: %s\nsubject: %s\n"
-     Dbpedia_record.Basic.(record.title)
-     Dbpedia_record.Basic.(record.abstract)
-     Dbpedia_record.Basic.(record.wiki_page)
-     Dbpedia_record.Basic.(Ptype.string_of_uri record.is_primary_topic_of)
-     Dbpedia_record.Basic.((string_of_list record.subject))
+  (Printf.printf "title: %s\nabstract: %s\nwiki_page_id: %s\nis_primary_topic_of: %s\nsubject: %s\n"
+     record.title
+     record.abstract
+     record.wiki_page_id
+     (Ptype.string_of_uri record.is_primary_topic_of)
+     (string_of_list record.subject)
   )
 
 let print_discography
@@ -91,12 +91,7 @@ let get_minimal_informations uri =
       (Rdf_uri.uri "http://dbpedia.org/sparql")
       Dbpedia_query.(minimal_informations_query.query)
   in
-  let format record =
-      record
-  in
-  let create_lightWeight =
-    let ret = Dbpedia_record.LightWeight.parse dbpedia_results in
-    List.map format ret
+  let create_lightWeight = Dbpedia_record.LightWeight.parse dbpedia_results
   in
   Lwt.return (if (is_wikipedia_uri uri) = false
   then raise (Dbpedia "Dbpedia_http.get_minimal_informations: \"uri\" parameter must be a wikipedia uri")
@@ -107,43 +102,17 @@ let get_minimal_informations uri =
 let get_basic_informations name =
   try_lwt
     let basic_query = Dbpedia_query.get_basic_query_infos name in
-    (* let basic_of_pair pairs = *)
-    (*   let title = get_value pairs "title" in *)
-    (*   let abstract = get_value pairs "abstract" in *)
-    (*   (\* let rdf_type = get_value pairs "type" in *\) *)
-    (*   let wiki_page = get_value pairs "wikiPage" in *)
-    (*   let is_primary_topic_of = get_value pairs "isPrimaryTopicOf" in *)
-    (*   let label = get_value pairs "label" in *)
-    (*   (\* let same_as = get_value pairs "sameAs" in *\) *)
-    (*   (title,abstract,[] (\* rdf_type *\),wiki_page,is_primary_topic_of, *)
-    (*    label,[] (\*sameAs*\)) *)
-    (* in *)
     lwt dbpedia_results = Rdf_http.query
         (Rdf_uri.uri "http://dbpedia.org/sparql")
         Dbpedia_query.(basic_query.query)
     in
-    let format record =
-      record
-      (* Dbpedia_record.Basic.(record.title, record.abstract, *)
-      (*                       record.wiki_page, record.is_primary_topic_of, *)
-      (*                       record.label) *)
-    in
     let ret = Dbpedia_record.Basic.parse dbpedia_results in
-    (* let pairs_list = (Rdf_http.pairs_of_solutions ~display:false *)
-    (* dbpedia_results Dbpedia_query.(basic_query.keys)) in *)
-    (* List.map basic_of_pair pairs_list *)
-    Lwt.return (List.map format ret)
+    Lwt.return (ret)
   with e -> raise (Dbpedia (get_exc_string e))
 
 let get_discography name =
   try_lwt
     let discography_query = Dbpedia_query.get_discography_query_infos name in
-    (* let discography_of_pair pairs = *)
-    (*   let song = get_value pairs "song" in *)
-    (*   let name = get_value pairs "song_name" in *)
-    (*   let album = get_value pairs "album" in *)
-    (*   (song, name, album) *)
-    (* in *)
     lwt dbpedia_results = Rdf_http.query
         (Rdf_uri.uri "http://dbpedia.org/sparql")
         Dbpedia_query.(discography_query.query)
@@ -152,8 +121,5 @@ let get_discography name =
       Dbpedia_record.Disco.(record.song, record.song_name, record.album)
     in
     let ret = Dbpedia_record.Disco.parse dbpedia_results in
-    (* let pairs_list = (Rdf_http.pairs_of_solutions ~display:false *)
-    (* dbpedia_results Dbpedia_query.(discography_query.keys)) in *)
-    (* List.map discography_of_pair pairs_list *)
     Lwt.return (List.map format ret)
   with e -> raise (Dbpedia (get_exc_string e))
