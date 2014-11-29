b3f5f5e2800dd2a3088fea55a573fa381a4f13e4
Paul 'Tweek' Roussel <ubik.arrakis@gmail.com>
2014-11-29 18:45:35 +0100

    fix fail commit

--- opencalais_http/opencalais_http.ml
+++ opencalais_http/opencalais_http.ml
@@ -47,4 +47,30 @@ let _ = set_token "z3k9bmug6udbqcqdwgt8qzq2"
 
 let body = "The Hobbit, or There and Back Again, is a fantasy novel and children's book by English author J. R. R. Tolkien. It was published on 21 September 1937 to wide critical acclaim, being nominated for the Carnegie Medal and awarded a prize from the New York Herald Tribune for best juvenile fiction. The book remains popular and is recognized as a classic in children's literature."
 
-lwt json_result = request ~display_body:true body
+lwt json_result = request ~display_body:false body
+
+let display_json json_tags = Yojson.Basic.pretty_to_string json_tags
+
+let get_json_list json_tags = Yojson.Basic.Util.to_list json_tags
+
+let list_displayer list_json = 
+  let displayer (name, json) = Printf.printf "%s - %s\n===\n" name (Yojson.Basic.pretty_to_string json) in 
+  List.iter displayer list_json
+
+let tags_from_results json_tags = 
+  let get_str_f_json json = Yojson.Basic.Util.to_string json in
+  let get_assoc json_tags = Yojson.Basic.Util.to_assoc json_tags in
+  let get_tags l json = 
+    if (get_str_f_json (Yojson.Basic.Util.member "_typeGroup" json)) == "socialTag"
+    then l::[(get_str_f_json (Yojson.Basic.Util.member "name" json))]
+    else l
+    in
+  List.fold_left get_tags [] (get_assoc json_tags)
+
+(* let parse_json result json_tags = result::[json_tags]
+
+let tags_from_results json_tags = Yojson.Basic.Util.to_list json_tags
+  List.fold_left parse_json [] json_tags *)
+
+let _ = list_displayer (Yojson.Basic.Util.to_assoc json_result);
+(* let _ = print_endline (display_json json_result); *)
