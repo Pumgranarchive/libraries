9641ec3aa04f6e29f273e2a72594925eb64712a8
Paul 'Tweek' Roussel <ubik.arrakis@gmail.com>
2015-01-01 15:49:45 +0100

    update doc about OpenCalais

--- opencalais_http/opencalais_http.mli
+++ opencalais_http/opencalais_http.mli
@@ -6,8 +6,10 @@
 (** set OpenCalais token *)
 val set_token : string -> unit
 
-(** get json structure from text [request ?display_body body_str] *)
+(** Ask OpenCalais API with a [string] content.
+The result is a [json] content with all OpenCalais informations about the [string].
+[display_body] is an option to display the result if set to true.*)
 val request : ?display_body:bool -> string -> Yojson.Basic.json Lwt.t
 
-(** get tags from a json list [tags_from_results json] *)
+(** fetch the social_tags attribute of [json] list. this function return a [string list] *)
 val tags_from_results : Yojson.Basic.json -> string list
