43968ca6fed8f5f2c761041c9899063778cd4579
Paul 'Tweek' Roussel <ubik.arrakis@gmail.com>
2015-01-01 15:33:48 +0100

    update new doc

--- opencalais_http/opencalais_http.mli
+++ opencalais_http/opencalais_http.mli
@@ -1,7 +1,13 @@
-(** [set_token token] *)
+(**
+   {b OpenCalais -
+   A ocaml OpenCalais binding}
+*)
+
+(** set OpenCalais token *)
 val set_token : string -> unit
 
-(** [request ?display_body body_str] *)
+(** get json structure from text [request ?display_body body_str] *)
 val request : ?display_body:bool -> string -> Yojson.Basic.json Lwt.t
 
+(** get tags from a json list [tags_from_results json] *)
 val tags_from_results : Yojson.Basic.json -> string list
