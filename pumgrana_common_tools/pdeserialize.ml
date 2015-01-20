17aa1b236a23eec5c6424a413655f72c7cf07197
ArnaudParant <ard.parant@gmail.com>
2015-01-20 23:47:27 +0100

    [Pumgrana tools] [Fix & Improve] Log

--- pumgrana_common_tools/pdeserialize.ml
+++ pumgrana_common_tools/pdeserialize.ml
@@ -1,5 +1,6 @@
 module Yojson = Yojson.Basic
 
+open Yojson
 open Yojson.Util
 open Pjson
 
@@ -8,6 +9,10 @@ exception Unknown_error of string
 exception Internal_server_error of string
 exception Bad_format of string
 
+(******************************************************************************
+********************************** Utils **************************************
+*******************************************************************************)
+
 let actions =
   [404,         (fun _ -> raise Not_found);
    500,         (fun err -> raise (Internal_server_error err))]
@@ -15,12 +20,41 @@ let actions =
 let string_of_unknown status err =
   (string_of_int status) ^ ": " ^ err
 
-let manage_bad_format str_err e json =
-  print_endline "\n\n";
-  print_endline (Printexc.to_string e);
-  print_endline "\n";
-  print_endline (to_string json);
-  raise (Bad_format str_err)
+let output_line oc line =
+  let line = line ^ "\n" in
+  output oc line 0 (String.length line)
+
+let options = [Open_wronly; Open_creat; Open_append; Open_text]
+let right = 0o666
+
+let datetime () =
+  let open Unix in
+  let tm = localtime (time ()) in
+  (string_of_int tm.tm_mday) ^ "/" ^
+  (string_of_int (tm.tm_mon + 1)) ^ "/" ^
+  (string_of_int (tm.tm_year + 1900)) ^ " " ^
+  (string_of_int tm.tm_hour) ^ ":" ^
+  (string_of_int tm.tm_min) ^ ":" ^
+  (string_of_int tm.tm_sec)
+
+let log filename subject exc description =
+  let now = datetime () in
+  let title = now ^ " #ERROR on " ^ subject in
+  let oc = open_out_gen options right filename in
+  output_line oc ("\n" ^ title);
+  output_line oc (Printexc.to_string exc);
+  if (String.length description > 0) then output_line oc description;
+  output_line oc "";
+  close_out oc;
+  title
+
+let manage_bad_format str_err exc json =
+  let title = log "pdeserialize.error" str_err exc (pretty_to_string json) in
+  raise (Bad_format title)
+
+(******************************************************************************
+******************************** Funtions *************************************
+*******************************************************************************)
 
 let get_service_return func json =
   let rec scan_status status err = function
