c6b4c0bd0c7437c8f0dc176e70efd4823ca45f1d
ArnaudParant <ard.parant@gmail.com>
2015-08-23 20:47:25 +0200

    [Ptools] Use Urlnorm for compare_uri

--- pumgrana_tools/ptype.ml
+++ pumgrana_tools/ptype.ml
@@ -27,7 +27,9 @@ let uri_of_string uri =
 let string_of_uri = Uri.to_string
 
 let compare_uri u1 u2 =
-  String.compare (string_of_uri u1) (string_of_uri u2)
+  let str_u1 = List.hd (Urlnorm.normalize [string_of_uri u1]) in
+  let str_u2 = List.hd (Urlnorm.normalize [string_of_uri u2]) in
+  String.compare str_u1 str_u2
 
 let replace remove_list replace_list str =
   let aux str remove_str replace_str =
@@ -50,40 +52,40 @@ let uri_decode url =
 *********************************** Test **************************************
 *******************************************************************************)
 
-(* module Test = *)
-(* struct *)
+module Test =
+struct
 
-(*   let compare () = *)
-(*     let uri_1 = uri_of_string "https://en.wikipedia.org/wiki/Astra_19.2°E" in *)
-(*     let uri_2 = uri_of_string "http://en.wikipedia.org/wiki/Astra_19.2°E" in *)
-(*     if (compare_uri uri_1 uri_2 == 0) *)
-(*     then print_endline "[Success]\t compare_uri" *)
-(*     else print_endline "[Fail]\t compare_uri" *)
+  let compare () =
+    let uri_1 = uri_of_string "https://en.wikipedia.org/wiki/Astra_19.2°E" in
+    let uri_2 = uri_of_string "http://en.wikipedia.org/wiki/Astra_19.2°E" in
+    if (compare_uri uri_1 uri_2 == 0)
+    then print_endline "[Success]\t compare_uri"
+    else print_endline "[Fail]\t compare_uri"
 
-(*   let cast () = *)
-(*     let should_succed uri = *)
-(*       try begin ignore (uri_of_string uri); true end *)
-(*       with Invalid_uri u -> false *)
-(*     in *)
-(*     let should_fail uri = not (should_succed uri) in *)
-(*     let good_string_uris = ["https://en.wikipedia.org/wiki/Astra_19.2°E"; *)
-(*                             "http://en.wikipedia.org/wiki/Astra_19.2°E"] *)
-(*     in *)
-(*     let bad_string_uris = ["fjkelwf://en.wikipedia.org/wiki/Astra_19.2°E"; *)
-(*                            "ipoo://en.wikipedia.org/wiki/Astra_19.2°E"] *)
-(*     in *)
-(*     let all_succed = List.for_all should_succed good_string_uris  in *)
-(*     let all_failed = List.for_all should_fail bad_string_uris  in *)
-(*     if (all_succed && all_failed) *)
-(*     then print_endline "[Success]\t cast_uri" *)
-(*     else print_endline "[Fail]\t cast_uri" *)
+  let cast () =
+    let should_succed uri =
+      try begin ignore (uri_of_string uri); true end
+      with Invalid_uri u -> false
+    in
+    let should_fail uri = not (should_succed uri) in
+    let good_string_uris = ["https://en.wikipedia.org/wiki/Astra_19.2°E";
+                            "http://en.wikipedia.org/wiki/Astra_19.2°E"]
+    in
+    let bad_string_uris = ["fjkelwf://en.wikipedia.org/wiki/Astra_19.2°E";
+                           "ipoo://en.wikipedia.org/wiki/Astra_19.2°E"]
+    in
+    let all_succed = List.for_all should_succed good_string_uris  in
+    let all_failed = List.for_all should_fail bad_string_uris  in
+    if (all_succed && all_failed)
+    then print_endline "[Success]\t cast_uri"
+    else print_endline "[Fail]\t cast_uri"
 
-(* end *)
+end
 
-(* let main () = *)
-(*   begin *)
-(*     Test.cast (); *)
-(*     Test.compare () *)
-(*   end *)
+let main () =
+  begin
+    Test.cast ();
+    Test.compare ()
+  end
 
-(* let () = main () *)
+let () = main ()
