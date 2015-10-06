13dd807fca8fc0b7e07d8f4e72294b686c407e18
ArnaudParant <ard.parant@gmail.com>
2015-10-06 23:02:07 +0200

    [boilerpipe] Install .jar with the ocaml lib

--- boilerpipe/boilerpipe.mli
+++ boilerpipe/boilerpipe.mli
@@ -9,7 +9,7 @@ exception Stopped of int
 
 type mode = Article | Default
 
-(** Set the jar path, default is 'jar/simpleboilerpipe.jar' *)
+(** Set the jar path, default is '%%JAR_PATH%%' *)
 val set_jar_path : string -> unit
 
 val boilerpipe : mode -> Uri.t -> string list Lwt.t
