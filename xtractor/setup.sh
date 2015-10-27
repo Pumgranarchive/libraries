1a9b0b82e8dfab6e41f85fcbe76dc742e9e6eb5f
ArnaudParant <ard.parant@gmail.com>
2015-10-27 11:08:56 +0100

    [Xtractor] use personal optimized branch

--- xtractor/setup.sh
+++ xtractor/setup.sh
@@ -5,8 +5,10 @@ JAVA_VERSION="1.8"
 if [ ! -d "lib/xtractor" ]; then
 
     mkdir -p lib
-    git clone git@github.com:mohaps/xtractor.git lib/xtractor
+    # git clone git@github.com:mohaps/xtractor.git lib/xtractor
+    git clone git@github.com:ArnaudParant/xtractor.git lib/xtractor
     cd lib/xtractor
+    git checkout optimized
     mvn clean install -Dmaven.compiler.source=$JAVA_VERSION -Dmaven.compiler.target=$JAVA_VERSION
 
 fi
